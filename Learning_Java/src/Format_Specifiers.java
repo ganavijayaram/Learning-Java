6e0920c819508fd23b2a7cb5889465359f11ab27
ganavee <itsganavijayaram@gmail.com>
2022-07-17 12:50:03 +0530

    Format specifiers

---
+++ Learning_Java/src/Format_Specifiers.java
@@ -0,0 +1,10 @@
+
+public class Format_Specifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double price = 19.8;
+		System.out.printf("$"+"%6.2f each",price);
+	}
+
+}
