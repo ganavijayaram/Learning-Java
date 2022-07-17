e4a997e368172b7f6cb858b2b40986ada19425cd
ganavee <itsganavijayaram@gmail.com>
2022-07-17 12:49:45 +0530

    Escape sequences

---
+++ Learning_Java/src/Escape_sequence.java
@@ -0,0 +1,10 @@
+
+public class Escape_sequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello World \rUsing Backslash";
+		System.out.println(s);
+	}
+
+}
